b3ffbdd777b6adea318d0ce4720fa8e996eecee4
Tony Tian <jt3640@columbia.edu>
2025-09-19 09:12:26 +0000

    implement wordguesser logic

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -3,11 +3,35 @@ class WordGuesserGame
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
+  attr_accessor :word, :guesses, :wrong_guesses
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter == '' || letter =~ /[^a-zA-Z]/
 
+    letter = letter.downcase
+
+    return false if @guesses.include?(letter) || @wrong_guesses.include?(letter)
+
+    if @word.include?(letter)
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+
+    return true
+  end
+  def word_with_guesses
+    @word.chars.map { |c| @guesses.include?(c) ? c : '-' }.join
+  end
+  def check_win_or_lose
+    return :win if word_with_guesses !~ /-/
+    return :lose if @wrong_guesses.length == 7
+    return :play
+  end
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
